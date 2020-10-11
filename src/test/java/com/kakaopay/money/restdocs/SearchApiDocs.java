dc03b8553454483eb833b4b73c36d7ec8a2d4397
shk3029 <Wjdwhdtjs4>
2020-10-11 16:53:47 +0900

    조회 api rest docs test code 추가

--- src/test/java/com/kakaopay/money/restdocs/SearchApiDocs.java
+++ src/test/java/com/kakaopay/money/restdocs/SearchApiDocs.java
@@ -1,53 +1,60 @@
-/*
-package com.kakaopay.money.docs;
+package com.kakaopay.money.restdocs;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kakaopay.money.constant.CustomHeaders;
-import com.kakaopay.money.share.dto.ShareDto;
+import com.kakaopay.money.constant.ShareType;
+import com.kakaopay.money.share.entity.Share;
+import com.kakaopay.money.share.service.ShareService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
+import org.springframework.restdocs.payload.JsonFieldType;
 
-import static org.springframework.restdocs.headers.HeaderDocumentation.*;
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.headers.HeaderDocumentation.requestHeaders;
 import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.linkWithRel;
 import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.links;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
-import static org.springframework.restdocs.payload.PayloadDocumentation.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 
 
 public class SearchApiDocs extends CommonApiDocs {
 
     @Autowired
-    private ObjectMapper objectMapper;
+    private ShareService shareService;
 
-    private ShareDto shareDto;
+    private Share share;
 
     @BeforeEach
-    @DisplayName("ShareDto 초기 셋팅")
+    @DisplayName("Share 인스턴스 생성")
     void setShareDto() {
-        shareDto = ShareDto.builder()
-                .money(10000L)
-                .count(3)
-                .build();
+        Share s = new Share();
+        s.setMoney(10000L);
+        s.setCount(3);
+        s.setUserId(1L);
+        s.setRoomId("a");
+        s.setShareType(ShareType.EQUITY);
+        share = shareService.share(s);
     }
 
     @Test
     @DisplayName("조회 API REST DOCS 생성 테스트 코드")
     void search() throws Exception {
-        this.mockMvc.perform(post("/api/share")
+        System.out.println(share);
+        this.mockMvc.perform(
+                RestDocumentationRequestBuilders.get("/api/share/{token}", share.getToken())
+                .accept(MediaTypes.HAL_JSON_VALUE)
                 .header(CustomHeaders.ROOM_ID, "a")
                 .header(CustomHeaders.USER_ID, 1L)
-                .contentType(MediaType.APPLICATION_JSON)
-                .accept(MediaTypes.HAL_JSON_VALUE)
-                .content(objectMapper.writeValueAsBytes(shareDto)))
-                .andDo(document("search-money",
+                .contentType(MediaType.APPLICATION_JSON))
+                .andDo(document("search",
                         preprocessRequest(
                                 prettyPrint()
                         ),
@@ -56,8 +63,6 @@ public class SearchApiDocs extends CommonApiDocs {
                         ),
                         links(
                                 linkWithRel("self").description("self"),
-                                linkWithRel("share").description("뿌리기"),
-                                linkWithRel("search").description("조회"),
                                 linkWithRel("profile").description("profile")
                         ),
                         requestHeaders(
@@ -66,29 +71,18 @@ public class SearchApiDocs extends CommonApiDocs {
                                 headerWithName(CustomHeaders.USER_ID).description("user_id header"),
                                 headerWithName(CustomHeaders.ROOM_ID).description("room_id header")
                         ),
-                        requestFields(
-                                fieldWithPath("money").description("금액"),
-                                fieldWithPath("count").description("인원")
-                        ),
-                        responseHeaders(
-                                headerWithName(CustomHeaders.LOCATION).description("location header"),
-                                headerWithName(CustomHeaders.CONTENT_TYPE).description("content type header")
+                        pathParameters(
+                                parameterWithName("token").description("토큰")
                         ),
                         responseFields(
-                                fieldWithPath("token").description("고유한 토큰값 (영문 3자리)"),
-                                fieldWithPath("user_id").description("사용자 id"),
-                                fieldWithPath("room_id").description("대화방 id"),
-                                fieldWithPath("money").description("금액"),
-                                fieldWithPath("count").description("인원"),
-                                fieldWithPath("shareType").description("뿌리는 타입"),
-                                fieldWithPath("receiveList").description("받은 사람들"),
-                                fieldWithPath("created_at").description("뿌린 날짜"),
+                                fieldWithPath("money").description("뿌린 금액"),
+                                fieldWithPath("createdAt").description("뿌린 시각"),
+                                fieldWithPath("receivedMoney").description("받기 완료된 금액"),
+                                fieldWithPath("receivedList").description("받기 완료된 정보[사용자ID, 받은금액]"),
                                 fieldWithPath("_links.self.href").description("_links.self"),
-                                fieldWithPath("_links.share.href").description("_links.share.href"),
-                                fieldWithPath("_links.search.href").description("_links.search.href"),
                                 fieldWithPath("_links.profile.href").description("_links.profile")
                         )
                 ));
     }
+
 }
-*/
