165233d70e87c95b0f36b5e7ff53b9862658ebca
171179 <171179@ssg.com>
2020-10-12 10:53:40 +0900

    Mapper 설정수정

--- src/main/java/com/kakaopay/money/share/mapper/ShareMapper.java
+++ src/main/java/com/kakaopay/money/share/mapper/ShareMapper.java
@@ -4,9 +4,10 @@ import com.kakaopay.money.share.dto.ShareDto;
 import com.kakaopay.money.share.entity.Share;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
 import org.mapstruct.factory.Mappers;
 
-@Mapper
+@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface ShareMapper {
     ShareMapper INSTANCE = Mappers.getMapper(ShareMapper.class);
 
